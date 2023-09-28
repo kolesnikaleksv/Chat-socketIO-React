bdb797d2663fee5dfc8b50022d57fc891ed56615
kolesnikaleksv <kolesnikaleksv@gmail.com>
2023-09-28 14:57:23 +0300

    Initial commit for client

---
+++ src/components/ChatBar.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+
+const ChatBar = ({socket}) => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    socket.on('newUserResponse', (data) => setUsers(data));
+  }, [socket, users]);
+  
+  console.log(users)
+  return (
+    <div className="chat__sidebar">
+      <h2>Open Chat</h2>
+      <div>
+        <h4 className="chat__header">ACTIVE USERS</h4>
+        <div className="chat__users">
+          <p>Number of users: {users.length}</p>
+          {/* <p>User 1</p>
+          <p>User 2</p>
+          <p>User 3</p>
+          <p>User 4</p> */}
+          {/* {
+            users.map((user) => {
+              return (
+                <p key={user.socketID}>{user.userName}</p>
+              )
+            })
+          } */}
+            {users.map((user) => (
+              <p key={user.socketID}>{user.userName}</p>
+            ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChatBar;
---
+++ src/components/ChatPage.js
@@ -0,0 +1,27 @@
+import React, {useEffect, useState} from 'react';
+import ChatBar from './ChatBar';
+import ChatBody from './ChatBody';
+import ChatFooter from './ChatFooter';
+
+const ChatPage = ({ socket }) => {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    socket.on('messageResponse', (data) => setMessages([...messages, data]));
+  }, [socket, messages]);
+
+  return (
+    <div className="chat">
+      {/* <ChatBar /> */}
+      <ChatBar socket={socket} />
+      <div className="chat__main">
+        {/* <ChatBody /> */}
+        <ChatBody messages={messages} />
+        {/* <ChatFooter /> */}
+        <ChatFooter socket={socket} />
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
